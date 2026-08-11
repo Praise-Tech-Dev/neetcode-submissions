683b0d3c3983977b18fd32a6b7102ab8c505319e
Praise Adeogun <spraiseadeogun@gmail.com>
2026-08-11 19:41:07 +0100

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): [][] {
+        const hashMap = new Map();
+
+        for (const str of strs) {
+            const key = str.split("").sort().join("");
+            if (!hashMap.has(key)){
+                hashMap.set(key, [])
+            }
+
+            hashMap.get(key).push(str)
+        }
+
+        return Array.from(hashMap.values())
+    }
+}
