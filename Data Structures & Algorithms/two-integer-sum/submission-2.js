0ac4a0c57fb6809fded73a64731ddb1b3fe2bdec
Praise Adeogun <spraiseadeogun@gmail.com>
2026-09-04 08:02:40 +0100

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const hash = new Map()
+
+        for (let i = 0; i < nums.length; i ++){
+            let compliment = target - nums[i]
+            if (hash.has(compliment)){
+                return [hash.get(compliment), i]
+            }
+
+            hash.set(nums[i], i)
+
+        }
+        return []
+    }
+}
