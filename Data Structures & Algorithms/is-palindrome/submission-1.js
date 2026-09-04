89b575ac409df7b6fe015bba2509a5abdfaa6b70
Praise Adeogun <spraiseadeogun@gmail.com>
2026-09-04 21:52:42 +0100

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let i = 0
+        let j = s.length - 1;
+        
+        while (i < j){
+            while (i < j && !/[a-zA-Z0-9]/.test(s[i])){
+                i ++
+            }
+            while (i < j && !/[a-zA-Z0-9]/.test(s[j])){
+                j --
+            }
+
+            if (s[i].toLowerCase() === s[j].toLowerCase()){
+                i ++
+                j--
+            }
+
+            else {
+                return false
+            }
+        }
+
+        return true
+    }
+}
